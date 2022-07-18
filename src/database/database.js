d24433e81b1cdbb4b91ecb625cfdcceffffb63c3
Rumira Daksith <rumirad@null.net>
2022-07-18 14:24:06 +0530

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -6,7 +6,8 @@ var connection = mysql.createConnection({
     host: process.env.SQL_HOST,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATABASE
+    database: process.env.SQL_DATABASE,
+    port: process.env.SQL_PORT || 3306,
 });
 
 connection.connect(function(err) {
